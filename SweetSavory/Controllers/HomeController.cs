adebe9ccdb8c2a0cf08f2c218ee91c39ab697551
Jason E. Church <elijahchurch@gmail.com>
2023-08-11 09:59:59 -0700

    Create route and view for home splash page

--- SweetSavory/Controllers/HomeController.cs
+++ SweetSavory/Controllers/HomeController.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using SweetSavory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace SweetSavory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SweetSavoryContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager, SweetSavoryContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            List<Treat> treats = _db.Treats.ToList();
+            List<Flavor> flavors = _db.Flavors.ToList();
+            ViewBag.Treats = treats;
+            ViewBag.Flavors = flavors;
+            ViewBag.Title = "Sweet and Savory";
+            return View();
+        }
+    }
+}
