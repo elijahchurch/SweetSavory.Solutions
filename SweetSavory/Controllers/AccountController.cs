58110325dab9bbbf758e13140b94b78de21c981d
Jason E. Church <elijahchurch@gmail.com>
2023-08-11 10:42:03 -0700

    Create Account Controller and ViewModels for Login and Register

---
+++ SweetSavory/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SweetSavory.Models;
+using System.Threading.Tasks;
+using SweetSavory.ViewModels;
+
+namespace SweetSavory.Controllers
+{
+    public class AccountController: Controller
+    {
+        private readonly SweetSavoryContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, SweetSavoryContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Title = "Register User";
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register (RegisterViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                ApplicationUser user = new ApplicationUser { UserName = model.Email};
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    Microsoft.AspNetCore.Identity.SignInResult signResult = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+                    if(signResult.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Login");
+                    }                    
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Title = "Login";
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else 
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "There is something wrong with your email. Please try again.");
+                        return View(model);
+                    }
+            }
+        }
+    }
+}
--- SweetSavory/Models/ApplicationUser.cs
+++ SweetSavory/Models/ApplicationUser.cs
@@ -4,6 +4,6 @@ namespace SweetSavory.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        
+
     }
 }
